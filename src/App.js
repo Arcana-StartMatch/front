e2cd6e21e70dd660123b2f3eb2693cbc11dc6620
Lord-forgos <johan.rousseau39@gmail.com>
2024-03-06 09:51:56 +0100

    hotfix des rebases

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Caroussel from "./Atoms/Caroussel";
+import Caroussel from "./Atoms/Caroussel/Caroussel";
 import MovieDescription from "./templates/FilmPresentation/MovieDescription";
 import RechercheFilm from './templates/RechercheFilm/RechercheFilm';
 import {Route, Routes} from 'react-router-dom';
@@ -8,9 +8,9 @@ import Formulaire from './templates/Formulaire/Formulaire';
 function App() {
     return (
         <Routes>
+            <Route path="/" element={<Formulaire/>}/>
             <Route path="/movie" element={<Caroussel/>}/>
             <Route path="/movie/:idMovie" element={<MovieDescription/>}/>
-            <Route path="/" element={<Formulaire/>}/>
             <Route path="/recherche" element={<RechercheFilm/>}/>
         </Routes>
     );
